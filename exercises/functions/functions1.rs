31eb35fe9d35b4015bb19be3acee4f5301067457
jaylen.chan <jaylen.chan@hotmail.com>
2022-02-24 13:28:58 +0800

    feat(functions): finish functions

--- exercises/functions/functions1.rs
+++ exercises/functions/functions1.rs
@@ -1,8 +1,9 @@
 // functions1.rs
 // Make me compile! Execute `rustlings hint functions1` for hints :)
 
-// I AM NOT DONE
-
 fn main() {
+    fn call_me() {
+        println!("call me");
+    }
     call_me();
 }
