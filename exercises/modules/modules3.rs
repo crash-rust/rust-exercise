1df22556f7dc31a98396c41e8f96604b082a2689
jaylen.chan <jaylen.chan@hotmail.com>
2022-02-24 14:29:35 +0800

    feat(modules): finish modules

--- exercises/modules/modules3.rs
+++ exercises/modules/modules3.rs
@@ -7,8 +7,8 @@
 
 // I AM NOT DONE
 
-// TODO: Complete this use statement
-use ???
+// Complete this use statement
+use std::time::{SystemTime, UNIX_EPOCH};
 
 fn main() {
     match SystemTime::now().duration_since(UNIX_EPOCH) {
